9823ef897c442f3b229a17d96881f07cabc79328
Kristoffer Risa <kristofferrisa@gmail.com>
2016-02-16 13:21:07 +0100

    Laget mappe struktur

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php $title = "eLearning"; include('html/header.html'); ?> 
+
+<?php include('html/menu.html'); ?> 
+
+<?php include('html/script.html'); ?>
+
+<?php include('html/footer.html'); ?>
+
