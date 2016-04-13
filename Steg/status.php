5b4b7817cee364138d6f1a9f894ebdc6c6d4a84e
Kristian Hov Bjerke <kristianhb87@gmail.com>
2016-04-13 13:18:34 +0200

    alltid smaa bokstaver i mappenavn

---
+++ Steg/status.php
@@ -0,0 +1,12 @@
+<?php $title = "eLearning"; ?>
+
+<!--?php include('html/menu.html'); ?-->
+<!-- include andre sider etter hvordan du vill ha siden til å se ut-->
+<!-- Hvilken html fill som du vill, dette vill bestemme innholdet ditt -->
+<?php include ('../html/header.html'); ?>
+
+<?php include ('../html/git-status.html') ?>
+
+<?php include ('../html/scripts.html'); ?>
+
+<?php include ('../html/footer.html'); ?>
